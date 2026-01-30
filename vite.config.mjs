70b5ca3da6ff0e0d8f114a441de77e5ec9552453
Anmol Singh <anmols990@gmail.com>
2026-01-30 14:20:38 +0530

    Reupload
    Reupload

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,22 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import tagger from "@dhiwise/component-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
-//   build: {
-//     outDir: "build",
-//     chunkSizeWarningLimit: 2000,
-//   },
-  base: "/anmol-portfolio/",
-  plugins: [tsconfigPaths(), react(), tagger()],
-  server: {
-    port: "4028",
-    host: "0.0.0.0",
-    strictPort: true,
-    allowedHosts: ['.amazonaws.com', '.builtwithrocket.new']
-  }
+  base: "./",
+  plugins: [
+    react(),
+    tsconfigPaths(),
+  ],
 });
